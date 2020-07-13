dafc439c5248cd816be1e0073c87156db69c9b20
samieSoa147 <sam789@live.com>
2020-07-13 01:54:23 -0400

    Produced flip feature on six cards

---
+++ app.js
@@ -0,0 +1,5 @@
+//  I want to create a six cards positioned relative to a parent div.
+
+//  After that I want to create html-text to represent the definition of the of the cards.
+// I then want to create ("p") within the div's that are displayed hidden within the divs.
+// I then want to create a function that toogles the paragraphs within all divs.
